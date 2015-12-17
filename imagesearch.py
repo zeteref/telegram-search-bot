d7e878b24638b6f7e9184f8bd39a6e9f957b19ca
zeteref <zeteref@live.com>
2015-12-17 11:48:39 +0100

    OH YEAH!

--- imagesearch.py
+++ imagesearch.py
@@ -12,24 +12,14 @@ def fetch_image(searchTerm):
     searchTerm = searchTerm.replace(' ','%20')
 
     url = ('http://www.bing.com/images/search?q=naked+asian')
-    request = urllib2.Request(url, None, {'Referer': 'testing'})
-    request.add_header('Cookie', 'SRCHHPGUSR=CW=1587&CH=371&DPR=1&ADLT=OFF')
-
-    response = urllib2.urlopen(url)
-    cookie = response.info()['set-cookie']
-    cookie += ', SRCHHPGUSR=CW=1587&CH=371&DPR=1&ADLT=OFF; domain=.bing.com; path=/; expires=Sat, 16-Dec-2017 09:49:05 GMT'
-
-    print cookie
-
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
-    req.add_header('Cookie',cookie)
-
-    page = urllib2.urlopen(req)
+    opener = urllib2.build_opener()
+    opener.addheaders.append(('Cookie', 'SRCHHPGUSR=CW=1587&CH=371&DPR=1&ADLT=OFF'))
+    page = opener.open(url)
     xhtml = page.read()
 
     page = pq(xhtml).xhtml_to_html()
 
+    print page('.thumb')
     for x in page('.thumb'): 
         print x.attrib['href']
 
